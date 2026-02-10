078e786c31e0c6b5d79e052a5a35cd5c54035d9d
Emirhan <emirhan.bda0@gmail.com>
2026-02-10 22:08:36 +0300

    .

--- antreman.js
+++ antreman.js
@@ -6,5 +6,15 @@ const db = [
     { id: 5, name: "Illia Topuria", age: 27 }
 ];
 
-const oneName = db.find(kisi => kisi.name==="Max Holloway");
-console.table(oneName);
+//"Bana yaşı 33'ten BÜYÜK olan dövüşçüleri getir. Gençlerle uğraşmak istemiyorum."
+// const yaslilar = db.filter(kisi => kisi.age > 33 );
+// console.table(yaslilar)
+
+//"Bana bu adamların yaşları, ID'leri lazım değil. Bana sadece İSİMLERİN olduğu basit bir liste ver. Ama hepsi BÜYÜK HARFLE yazılsın."
+// const isimler = db.map(isim => isim.name.toUpperCase());
+// console.table(isimler)
+
+//"Bu adamın listedeki TÜM BİLGİLERİNİ (Objeyi) bana bul getir. Dizinin içinde gez, onu bulduğun an dur ve bana ver."
+// const oneName = db.find(kisi => kisi.name==="Max Holloway");
+// console.table(oneName);
+
