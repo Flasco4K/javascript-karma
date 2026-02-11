5fe593b8bea6a8a63c756e16d4133619fdc32d62
Emirhan <emirhan.bda0@gmail.com>
2026-02-12 00:09:32 +0300

    .

--- Modern-JS-ES6/sorular.js
+++ Modern-JS-ES6/sorular.js
@@ -20,5 +20,34 @@ const profile = {
 };
 
 //Soru 1: Profil sayfasında kullanıcının Şehri ve Posta Kodunu göstermemiz lazım.
-const { location: { city, address: { zip } } } = profile;
-console.log(city,zip);
+// const { location: { city, address: { zip } } } = profile;
+// console.log(city,zip);
+
+//Soru 2: İSİM DEĞİŞTİRME & DİZİLER
+// const { fullName: adSoyad, posts:[ilkPost] } = profile;
+// console.log(adSoyad);
+// console.log(ilkPost);
+
+//Soru 3: PROFİL GÜNCELLEME (SPREAD KOMBO)
+//profile objesini bozmadan, guncelProfile adında yeni bir obje oluşturman. Bu yeni objede şu değişiklikler olsun:
+//Şehir Değişsin "Amsterdam" olsun. Yeni Post Eklensin 3. bir post ekle
+
+// const guncelProfile = {
+//     ...profile,
+//     location:{
+//         ...profile.location,
+//         city:"Amsterdam"
+//     },
+//     posts:[
+//         ...profile.posts,
+//         {
+//             id:3,
+//             title:"Hollandada Yazılımcı Olmak",
+//             likes:500
+//         }
+//     ]
+// };
+// console.log("--- GÜNCEL PROFİL ---");
+// console.log(guncelProfile);
+
+//Soru 4: 
