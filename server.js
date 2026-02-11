87c09ae9104ae4c5799738f703aa165a33d51627
Emirhan <emirhan.bda0@gmail.com>
2026-02-11 16:06:58 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const siparisler = [
+    { id: 1001, musteri: "Ahmet Yilmaz", tutar: 1500, durum: "Teslim Edildi", items: ["Klavye", "Mouse"] },
+    { id: 1002, musteri: "Ayşe Demir", tutar: 45000, durum: "İptal", items: ["Laptop"] },
+    { id: 1003, musteri: "Mehmet Öz", tutar: 350, durum: "Kargoda", items: ["HDMI Kablo", "Temizleme Kiti"] },
+    { id: 1004, musteri: "Ahmet Yilmaz", tutar: 2000, durum: "Hazirlaniyor", items: ["Monitör"] },
+    { id: 1005, musteri: "Zeynep Kaya", tutar: 8000, durum: "Teslim Edildi", items: ["Tablet", "Kilif"] }
+];
+
+//Soru 1:
